0aa338bd127ce040f5a5a65f682a5e89c79f0f45
Divyanshu Kumar <divyanshuk9060@gmail.com>
2023-09-20 18:49:49 +0530

    Commit Package

---
+++ src/com/java/comment/Comment.java
@@ -0,0 +1,9 @@
+package com.java.comment;
+
+public class Comment {
+    public static void main(String[] args) {
+        //This is comment
+        System.out.println("Hello World");
+        //System.out.println("Hello");
+    }
+}
