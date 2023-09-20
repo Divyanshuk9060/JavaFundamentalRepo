12c18a9590f3fe55bb7d909b281366007ce89a83
Divyanshu Kumar <divyanshuk9060@gmail.com>
2023-09-20 19:07:45 +0530

    Commit

--- src/com/java/method/Method.java
+++ src/com/java/method/Method.java
@@ -1,4 +1,8 @@
 package com.java.method;
 
 public class Method {
+    public static void main(String[] args) {
+
+
+    }
 }
---
+++ src/com/java/variables/Variables.java
@@ -0,0 +1,24 @@
+package com.java.variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Divyanshu";
+        System.out.println(name);
+
+        //Variable assign it the value 15:
+        int myNum = 10;
+        System.out.println(myNum);
+
+        //Variable without assigning the value
+        int myNum1;
+        myNum1 = 24;
+        System.out.println(myNum1);
+
+        //Assign a new value to an existing variable
+        int myNum2 = 50;
+        myNum2 = 60;  // myNum is now 60
+        System.out.println(myNum2);
+
+
+    }
+}
